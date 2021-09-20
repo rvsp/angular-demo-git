dc2468043e3c442a5307a0796f1dee021fc0b0f5
Venkatasubramanian <pvenkat.rv@gmail.com>
2021-09-20 20:08:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo-directives';
+  userName: any;
+
+  obj = [
+    {
+      name: 'Dell Laptop',
+      price: '$1299',
+      stock: true,
+      image:
+        'https://images.prismic.io/frameworkmarketplace/cca31de3-3b75-4932-af96-7646b7eba6c7__DSC3630-Edit-cropped.jpg?auto=compress,format',
+    },
+    {
+      name: 'HP Laptop',
+      price: '$1199',
+      stock: true,
+      image: 'https://m.media-amazon.com/images/I/81SdJG3OUjL._SL1500_.jpg',
+    },
+    {
+      name: 'Mouse',
+      price: '$199',
+      stock: false,
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/2/22/3-Tasten-Maus_Microsoft.jpg',
+    },
+  ];
 }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  // @Input('imageUrl') imageUrl: any;
+  // @Input('text') text: any;
+  @Input('item') item: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
